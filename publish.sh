b22ac7a4c0cf8503c34bfa833b48342de8123526
Łukasz Kwinta <lukaszmind@gmail.com>
2025-09-10 13:22:15 +0200

    workings compile and publish scripts

--- publish.sh
+++ publish.sh
@@ -7,7 +7,7 @@ python -m pip install s3cmd
 source setup_env.sh
 
 rclone mkdir geant4_web:geant4-wasm
-rclone sync $MEMFS/bucket-staging/geant4-wasm geant4_web:geant4-wasm -p
+rclone sync $MEMFS/bucket-staging/geant4-wasm geant4_web:geant4-wasm -P
 
 s3cmd -c .s3cfg setcors cors.xml s3://geant4-wasm 
 s3cmd -c .s3cfg setpolicy policy.json s3://geant4-wasm 
